4f85e8af080ba1a3cd3b9f8b391562e3bf5f2435
Bartosz Łuka <barl@netcompany.com>
2025-02-17 07:55:52 +0100

    fix(lualine): looks better now

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -2,112 +2,84 @@ return {
     "nvim-lualine/lualine.nvim",
     event = "VeryLazy",
     config = function()
-        local theme = vim.g.colors_name
-
-        local function fg(name)
-            return function()
-                ---@type {foreground?:number}?
-                local hl = vim.api.nvim_get_hl_by_name(name, true)
-                return hl and hl.foreground and { fg = string.format("#%06x", hl.foreground) }
-            end
+        local function lsp()
+            local buf_id = vim.api.nvim_get_current_buf()
+            local lsps = vim.lsp.get_clients({ bufnr = buf_id })
+            local lsp_names = vim.iter(lsps)
+                :map(function(server)
+                    return server.name
+                end)
+                :join("+")
+            return lsp_names
         end
+        local theme = vim.g.colors_name or "auto"
         require("lualine").setup({
             options = {
                 icons_enabled = true,
                 theme = theme,
                 component_separators = "|",
                 section_separators = "",
-                disabled_filetypes = {
-                    statusline = {
-                        "dashboard",
-                        "lazy",
-                        "alpha",
-                        "TelescopePrompt",
-                    },
-                    winbar = {},
-                },
-                ignore_focus = {},
-                always_divide_middle = true,
-                globalstatus = true,
-                refresh = {
-                    statusline = 1000,
-                    tabline = 1000,
-                    winbar = 1000,
-                },
             },
             sections = {
-                lualine_a = { "mode" },
+                lualine_a = {
+                    "mode",
+                },
                 lualine_b = {
                     "branch",
-                    {
-                        "diff",
-                        symbols = {
-                            added = " ",
-                            modified = " ",
-                            removed = " ",
-                        },
-                    },
                 },
                 lualine_c = {
                     {
                         "filename",
-                        path = 1,
-                        newfile_status = true,
-                        -- symbols = {
-                        --     modified = "●",
-                        --     readonly = "",
-                        --     unnamed = "",
-                        -- },
-                        color = { gui = "bold" },
+                        file_status = true, -- Displays file status (readonly status, modified status)
+                        newfile_status = true, -- Display new file status (new file means no write after created)
+                        path = 4,
+                        color = "Identifier"
+                        -- 0: Just the filename
+                        -- 1: Relative path
+                        -- 2: Absolute path
+                        -- 3: Absolute path, with tilde as the home directory
+                        -- 4: Filename and parent dir, with tilde as the home directory
                     },
                 },
                 lualine_x = {
                     {
-                        "diagnostics",
-                        symbols = {
-                            error = " ",
-                            warn = " ",
-                            info = " ",
-                            hint = " ",
-                        },
+                        lsp,
+                        icon = " ",
+                        color = "@function.call",
                     },
                     {
-                        function()
-                            local clients = vim.lsp.get_clients({ bufnr = 0 })
-
-                            local just_names = vim.iter(clients)
-                                :map(function(client)
-                                    return client.name
-                                end)
-                                :totable()
-                            return table.concat(just_names, ", ")
-                        end,
-                        icon = " ",
-                        color = fg("@function.call"),
+                        "diagnostics",
+                        sources = { "nvim_diagnostic" },
+                        colored = true,
                     },
                 },
                 lualine_y = {
+                    -- "fileformat",
+                    "filetype",
                     "encoding",
-                    { "filetype", icon_only = false },
                 },
-                lualine_z = { "location" },
+                lualine_z = {
+                    {
+                        "location",
+                        "selectioncount",
+                    },
+                },
             },
             inactive_sections = {
-                lualine_a = {},
-                lualine_b = {},
-                lualine_c = { "filename" },
-                lualine_x = { "location" },
-                lualine_y = {},
-                lualine_z = {},
+                lualine_b = {
+                    {
+                        "filename",
+                        path = 0,
+                        status = true,
+                    },
+                },
+                lualine_x = { "filetype" },
             },
             tabline = {},
-            winbar = {},
-            inactive_winbar = {},
             extensions = {
-                "fugitive",
                 "quickfix",
                 "oil",
-                "trouble",
+                "toggleterm",
             },
         })
     end,
