3d8ab91dda41ca24d877bb3bd83cff4bdaeabbe9
Bartosz Łuka <barl@netcompany.com>
2025-02-18 11:06:57 +0100

    feat(mini.ai): use treesitter for parameters

--- lua/plugins/mini-ai.lua
+++ lua/plugins/mini-ai.lua
@@ -10,6 +10,10 @@ return {
                     a = { "@block.outer", "@conditional.outer", "@loop.outer" },
                     i = { "@block.inner", "@conditional.inner", "@loop.inner" },
                 }),
+                a = ai.gen_spec.treesitter({
+                    a = { "@parameter.outer" },
+                    i = { "@parameter.inner", "@parameter.outer" },
+                }),
                 f = ai.gen_spec.treesitter({ a = "@function.outer", i = "@function.inner" }),
                 C = ai.gen_spec.treesitter({ a = "@class.outer", i = "@class.inner" }),
                 r = ai.gen_spec.pair("[", "]", { type = "balanced" }),
